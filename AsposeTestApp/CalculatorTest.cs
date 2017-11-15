d31fd9eadda4e7903bcbe5e1ac5af85dfcc36b9d
Denis Schelkunov <denis@aviata.me>
2017-11-15 20:27:45 +0600

    Добавьте файлы проекта.

---
+++ AsposeTestApp/CalculatorTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace AsposeTestApp
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        [TestCase]
+        public void AddTest()
+        {
+            Calculator helper = new Calculator();
+            decimal result = helper.Addition(20, 10);
+            Assert.AreEqual(30, result);
+        }
+
+        [TestCase]
+        public void SubtractTest()
+        {
+            Calculator helper = new Calculator();
+            decimal result = helper.Subtraction(20, 10);
+            Assert.AreEqual(10, result);
+        }
+    }
+}
---
+++ AsposeTestApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsposeTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            Console.WriteLine(calc.Addition(109, 11));
+            Console.ReadKey();
+        }
+    }
+}
